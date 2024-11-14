d620dd0eb86c330c6b287fc8f72deec447de4497
Char <143526767+char667v@users.noreply.github.com>
2024-11-14 08:56:43 +0100

    footer same width as header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,23 +3,4 @@ import { CssSyntaxError } from 'postcss';
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			primary:'#212121', /* sort */ /* DONE */
-			secondary: '#FFDA03',/* neon gul */ /* DONE */
-		},
-		extend: {
-			spacing: {
-				'58': '230px',
-				'100': '25rem',
-				'128': '39rem',
-				'130': '44rem',
-			},
-			height: {
-				'0.25': '0.25px',  // Custom height value
-			},
-		},
-	},
-	variants: {},
-	plugins: [],
 };
